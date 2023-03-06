ad81270d7741c8d666065dce8fc915505f30c7df
Evgenii Baburin <r3drum@yandex.ru>
2023-03-07 00:11:46 +0300

    Применил анимацию перемещения между фрагментами

--- app/src/main/java/ru/eababurin/pictureoftheday/view/navigation/EarthFragment.kt
+++ app/src/main/java/ru/eababurin/pictureoftheday/view/navigation/EarthFragment.kt
@@ -5,11 +5,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.viewpager2.widget.ViewPager2
 import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
 import ru.eababurin.pictureoftheday.R
 import ru.eababurin.pictureoftheday.databinding.FragmentEarthBinding
 import ru.eababurin.pictureoftheday.view.adapters.ViewPager2Adapter
+import kotlin.math.abs
 
 class EarthFragment : Fragment() {
 
@@ -28,7 +30,41 @@ class EarthFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.viewPager.adapter = ViewPager2Adapter(requireActivity())
+        binding.viewPager.apply {
+            adapter = ViewPager2Adapter(requireActivity())
+            setPageTransformer(object : ViewPager2.PageTransformer {
+                override fun transformPage(page: View, position: Float) {
+                    page.apply {
+                        val pageWidth = width
+                        when {
+                            position < -1 -> {
+                                alpha = 0f
+                            }
+                            position <= 0 -> {
+                                alpha = 1f
+                                translationX = 0f
+                                translationZ = 0f
+                                scaleX = 1f
+                                scaleY = 1f
+                            }
+                            position <= 1 -> {
+                                alpha = 1 - position
+                                translationZ = -1f
+                                translationX = pageWidth * -position
+
+                                val scaleFactor = (0.75f + (1 - 0.75f) * (1 - abs(position)))
+                                scaleX = scaleFactor
+                                scaleY = scaleFactor
+                            }
+                            else -> {
+                                alpha = 0f
+                            }
+                        }
+                    }
+                }
+
+            })
+        }
 
         TabLayoutMediator(
             binding.tabLayout,
